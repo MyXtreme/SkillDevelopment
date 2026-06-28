cce6efeb7af0bb6181a3029a842935923385b317
Mukhtar <mn5feterless@gmail.com>
2026-06-28 09:24:44 +0500

    add utils for text

---
+++ src/features/typingExperience/utils/textUtils.ts
@@ -0,0 +1,18 @@
+import commonWords from "../../../data/commonWordsEng";
+
+export function randomTexGeneration(
+  source: string[] = commonWords,
+  wordRange = 40,
+): string {
+  const dataset: string[] = source;
+
+  let index: number;
+  const min: number = 0;
+  const max: number = dataset.length;
+  let text: string = "";
+  for (let i = 0; i < wordRange; i++) {
+    index = Math.floor(Math.random() * (max - min)) + min;
+    text += dataset[index] + " ";
+  }
+  return text;
+}
